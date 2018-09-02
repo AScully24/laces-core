6f78af3fee135b9367f082503c73ad2f83130335
AScully24 <AScully24@users.noreply.github.com>
2018-09-02 12:38:29 +0100

    Minor changes and refactoring (#5)
    Removed reference to ADT in registration email

--- src/main/kotlin/com/laces/core/email/EmailService.kt
+++ src/main/kotlin/com/laces/core/email/EmailService.kt
@@ -1,25 +1,19 @@
 package com.laces.core.email
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Component
 
-
 @Component
-class EmailService {
-
-    @Autowired
-    lateinit var emailSender: JavaMailSender
-
+class EmailService(
+        val emailSender: JavaMailSender
+) {
     fun sendSimpleMessage(to: String, subject: String, text: String) {
-
         val message = SimpleMailMessage()
         message.setTo(to)
         message.subject = subject
         message.from = "testing@something.com"
         message.text = text
         emailSender.send(message)
-
     }
 }
