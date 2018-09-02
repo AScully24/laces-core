bf0459156fc489973d71940130120ba907134a63
AScully24 <AScully24@users.noreply.github.com>
2018-09-02 12:14:40 +0100

    Closes #1 Email validator now implemented (#4)

--- src/main/kotlin/com/laces/core/security/component/register/RegisterService.kt
+++ src/main/kotlin/com/laces/core/security/component/register/RegisterService.kt
@@ -1,13 +1,14 @@
 package com.laces.core.security.component.register
 
 import com.laces.core.email.EmailService
-import com.laces.core.responses.UserRegistrationTokenException
+import com.laces.core.responses.*
 import com.laces.core.security.component.passkey.KeyGeneratorService
 import com.laces.core.security.component.user.NewUser
 import com.laces.core.security.component.user.User
 import com.laces.core.security.component.user.UserService
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.validator.routines.EmailValidator
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -15,30 +16,25 @@ import java.util.*
 import javax.transaction.Transactional
 
 @Service
-class RegisterService {
+class RegisterService(
+        val registerTokenRepository: RegisterTokenRepository,
+        val userService: UserService,
+        val keyGeneratorService: KeyGeneratorService,
+        val emailService: EmailService,
+
+        @Value("\${app.url}")
+        val appUrl: String
+) {
+
+    val emailValidator: EmailValidator = EmailValidator.getInstance(false)
 
     companion object {
         private val LOG = LoggerFactory.getLogger(RegisterService::class.java)
     }
 
-    @Autowired
-    lateinit var registerTokenRepository: RegisterTokenRepository
-
-    @Autowired
-    lateinit var userService: UserService
-
-    @Autowired
-    lateinit var keyGeneratorService : KeyGeneratorService
-
-    @Autowired
-    lateinit var emailService : EmailService
-
-    @Value("\${app.url}")
-    lateinit var appUrl : String
-
     @Transactional
     fun registerNewUser(newUser: NewUser): User {
-        userService.validateNewUser(newUser)
+        validateNewUser(newUser)
 
         val user = userService.saveNewUser(newUser, false)
 
@@ -56,10 +52,30 @@ class RegisterService {
 
     }
 
+    fun validateNewUser(newUser: NewUser) {
+
+        if (userService.existsByName(newUser.username)) {
+            throw EmailExistsException("Email already exists: ${newUser.username}")
+        }
+
+        if (!emailValidator.isValid(newUser.username)) {
+            throw InvalidEmailException("Email is not in a valid format: ${newUser.username}")
+        }
+
+        if (StringUtils.isBlank(newUser.password)) {
+            throw EmptyPasswordException("Password cannot be blank.")
+        }
+
+        if (newUser.password != newUser.confirmPassword) {
+            throw PasswordMismatchException("Passwords do not match.")
+        }
+    }
+
+
     @Transactional
     fun completeRegistration(token: String) {
 
-        if (!registerTokenRepository.existsByToken(token)){
+        if (!registerTokenRepository.existsByToken(token)) {
             throw UserRegistrationTokenException(token)
         }
 
@@ -71,7 +87,7 @@ class RegisterService {
 
     @Transactional
     @Scheduled(cron = "\${app.unvalidatedUserCron}")
-    fun removeUnvalidatedNewUsersAndRegisterTokens(){
+    fun removeUnvalidatedNewUsersAndRegisterTokens() {
         val today = Date()
         val oldTokens = registerTokenRepository.findAllByExpiryDateLessThan(today)
         val oldUsers = oldTokens
--- src/main/kotlin/com/laces/core/security/controllers/SecurityController.kt
+++ src/main/kotlin/com/laces/core/security/controllers/SecurityController.kt
@@ -3,7 +3,6 @@ package com.laces.core.security.controllers
 import com.laces.core.security.component.register.RegisterService
 import com.laces.core.security.component.user.NewUser
 import com.laces.core.security.component.user.UserService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -11,28 +10,23 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("auth")
-class SecurityController {
-
-    @Autowired
-    lateinit var userService : UserService
-
-    @Autowired
-    lateinit var registerService: RegisterService
+class SecurityController(
+        val userService: UserService,
+        val registerService: RegisterService
+) {
 
     @PostMapping("success")
-    fun success() : ResponseEntity<Any> = ResponseEntity.ok().build ()
+    fun success(): ResponseEntity<Any> = ResponseEntity.ok().build()
 
     @PostMapping("failure")
-    fun failure() : ResponseEntity<Any> = ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+    fun failure(): ResponseEntity<Any> = ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
 
     @GetMapping("logout")
-    fun logout() : ResponseEntity<Any> = ResponseEntity.ok().build()
+    fun logout(): ResponseEntity<Any> = ResponseEntity.ok().build()
 
-    @GetMapping(value = ["validate"],consumes = [(MediaType.APPLICATION_JSON_VALUE)])
-    fun validateUser(@RequestBody newUser: NewUser) : ResponseEntity<HashMap<String, String>>? {
-        this.userService.validateNewUser(newUser)
-
-        return ResponseEntity.ok().build()
+    @GetMapping(value = ["validate"], consumes = [(MediaType.APPLICATION_JSON_VALUE)])
+    fun validateUser(@RequestBody newUser: NewUser) {
+        registerService.validateNewUser(newUser)
     }
 
     @PostMapping(value = ["register"])
@@ -42,10 +36,10 @@ class SecurityController {
     }
 
     @GetMapping("/logged-in")
-    fun isLoggedIn(): ResponseEntity<Any>{
-        if (this.userService.isUserLoggedIn()){
+    fun isLoggedIn(): ResponseEntity<Any> {
+        if (this.userService.isUserLoggedIn()) {
             return ResponseEntity.ok().build()
         }
-        return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
     }
 }
