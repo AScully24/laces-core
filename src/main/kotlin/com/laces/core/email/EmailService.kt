8babd1cc7ce8d7aababf5ee5bb47d32772dfdce8
AScully24 <179324865a!>
2019-03-27 12:56:17 +0000

    Dev able to define from address for registration emails.

--- src/main/kotlin/com/laces/core/email/EmailService.kt
+++ src/main/kotlin/com/laces/core/email/EmailService.kt
@@ -1,18 +1,21 @@
 package com.laces.core.email
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Component
 
 @Component
 class EmailService(
-        val emailSender: JavaMailSender
+        val emailSender: JavaMailSender,
+        @Value("\${laces.security.registration.from-email}")
+        val fromEmail: String
 ) {
     fun sendSimpleMessage(to: String, subject: String, text: String) {
         val message = SimpleMailMessage()
         message.setTo(to)
         message.subject = subject
-        message.from = "testing@something.com"
+        message.from = fromEmail
         message.text = text
         emailSender.send(message)
     }
