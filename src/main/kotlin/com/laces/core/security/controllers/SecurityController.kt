4eb49de8192c468c70b5491129202ce4f0b7c37f
AScully24 <anthony.scully24@gmail.com>
2020-03-01 09:46:02 +0000

    Improved performance searching for form classes
    Changed isLoggedIn logic

--- src/main/kotlin/com/laces/core/security/controllers/SecurityController.kt
+++ src/main/kotlin/com/laces/core/security/controllers/SecurityController.kt
@@ -3,11 +3,12 @@ package com.laces.core.security.controllers
 import com.laces.core.security.component.register.RegisterService
 import com.laces.core.security.component.user.NewUser
 import com.laces.core.security.component.user.UserService
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
+import org.springframework.http.ResponseEntity.status
 import org.springframework.web.bind.annotation.*
-import java.lang.RuntimeException
 
 @RestController
 @RequestMapping("auth")
@@ -17,12 +18,12 @@ class SecurityController(
 ) {
 
     @PostMapping("success")
-    fun success(): Map<String,String> {
+    fun success(): Map<String, String> {
         return mapOf("subscriptionState" to userService.getCurrentUser().subscriptionState.name)
     }
 
     @GetMapping("logout")
-    fun logout(): ResponseEntity<Any> = ResponseEntity.ok().build()
+    fun logout(): ResponseEntity<Any> = ok().build()
 
     @GetMapping(value = ["validate"], consumes = [(MediaType.APPLICATION_JSON_VALUE)])
     fun validateUser(@RequestBody newUser: NewUser) {
@@ -37,12 +38,10 @@ class SecurityController(
 
     @GetMapping("/logged-in")
     fun isLoggedIn(): ResponseEntity<Any> {
-        try {
-            if (userService.isUserLoggedIn()) {
-                return ResponseEntity.ok().build()
-            }
-        }catch (re : RuntimeException){}
+        if (userService.isUserLoggedIn()) {
+            return ok().build()
+        }
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+        return status(UNAUTHORIZED).build()
     }
 }
