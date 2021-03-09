73d13bc3faca498b851d6807f1a7d50afae4e845
AScully24 <anthony.scully24@gmail.com>
2021-03-09 08:03:54 +0000

    User plan now updates and displays correctly

--- src/main/kotlin/com/laces/core/security/component/payment/plans/user/UserPlanService.kt
+++ src/main/kotlin/com/laces/core/security/component/payment/plans/user/UserPlanService.kt
@@ -2,8 +2,9 @@ package com.laces.core.security.component.payment.plans.user
 
 import com.laces.core.responses.UserSubscriptionStripeIdException
 import com.laces.core.security.component.payment.plans.SubscriptionPlanService
-import com.laces.core.security.component.user.subscription.SubscriptionState
+import com.laces.core.security.component.user.User
 import com.laces.core.security.component.user.UserService
+import com.laces.core.security.component.user.subscription.SubscriptionState
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,12 +13,12 @@ class UserPlanService (
         val userService: UserService
 ){
     // Change the controller that calls this to use a DTO object. this will allow consistency if this ever needs to be changed
-    fun getCurrentUserPlan() : UserPlan {
-        val user = userService.getCurrentUser()
+    fun getCurrentUserPlan() : Pair<User, UserPlan> {
+        val user = userService.findById(userService.getCurrentUser().id!!) ?: throw RuntimeException("Unable to find user")
         val plan = subscriptionPlanService.findSubscriptionPlan(user.planStripeId)
                 ?: throw UserSubscriptionStripeIdException("Unable to find subscription ID ${user.planStripeId} for user ${user.username}")
-        return UserPlan(plan.name,
-                user.subscriptionState == SubscriptionState.CANCEL_PENDING,
-                user.subscriptionState)
+        return Pair(user, UserPlan(plan.name,
+            user.subscriptionState == SubscriptionState.CANCEL_PENDING,
+            user.subscriptionState))
     }
 }
