06629d6835acc3153a9452f7c33cdd18c62a6b1f
AScully24 <anthony.scully24@gmail.com>
2021-03-30 11:07:34 +0100

    Cleaned up some code

--- src/main/kotlin/com/laces/core/security/component/user/subscription/UserSubscriptionStatusService.kt
+++ src/main/kotlin/com/laces/core/security/component/user/subscription/UserSubscriptionStatusService.kt
@@ -3,7 +3,6 @@ package com.laces.core.security.component.user.subscription
 import com.laces.core.security.component.user.User
 import com.laces.core.security.component.user.UserService
 import com.laces.core.security.component.user.subscription.SubscriptionState.*
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
@@ -11,19 +10,13 @@ import javax.transaction.Transactional
 class UserSubscriptionStatusService(
         val userService: UserService
 ) {
-    companion object {
-        private val LOG = LoggerFactory.getLogger(UserSubscriptionStatusService::class.java)
-    }
-
     @Transactional
     fun cancelPendingUserSubscription(user: User) {
-        LOG.info("Cancelling user subscription: ${user.id}")
         userService.save(user.copy(subscriptionState = CANCEL_PENDING))
     }
 
     @Transactional
     fun setUserSubscriptionToUnpaid(user: User) {
-        LOG.info("User subscription not paid: ${user.id}")
         val updatedUser = user.copy(subscriptionState = UNPAID)
         userService.save(updatedUser)
         userService.expireUserSessions(user)
@@ -31,7 +24,6 @@ class UserSubscriptionStatusService(
 
     @Transactional
     fun setUserSubscriptionToActive(user: User) {
-        LOG.info("User subscription activated: ${user.id}")
         userService.save(user.copy(subscriptionState = ACTIVE))
     }
 
